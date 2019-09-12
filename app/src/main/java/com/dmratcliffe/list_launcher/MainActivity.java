c662f3a19d5f0eada333470f24d019a4faf97c5b
dmratcliffe <dmrat@dmratcliffe.com>
2019-09-12 18:29:16 -0500

    Function stubs for buttons, main layout WIP

--- app/src/main/java/com/dmratcliffe/list_launcher/MainActivity.java
+++ app/src/main/java/com/dmratcliffe/list_launcher/MainActivity.java
@@ -3,12 +3,52 @@ package com.dmratcliffe.list_launcher;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.ImageButton;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
 public class MainActivity extends AppCompatActivity {
+    boolean debug = true;
+    String TAG = "list-launcher";
 
+    private ImageButton settingsButton, searchButton;
+    private FloatingActionButton appDrawerButton;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        settingsButton = findViewById(R.id.settingsButton);
+        searchButton = findViewById(R.id.searchButton);
+
+        appDrawerButton = findViewById(R.id.appDrawerButton);
+
+
+        settingsButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view)
+            {
+                if(debug)
+                    Log.i(TAG, "onClick: settingsButton was pressed, moving activity");
+            }
+        });
+        searchButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view)
+            {
+                if(debug)
+                    Log.i(TAG, "onClick: searchButton was pressed, moving activity");
+            }
+        });
+        appDrawerButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view)
+            {
+                if(debug)
+                    Log.i(TAG, "onClick: appDrawerButton button was pressed, moving activity");
+            }
+        });
     }
 }
